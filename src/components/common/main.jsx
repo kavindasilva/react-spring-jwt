50899dffd8d1fe0a606d1798174433e5e86ed49c
kavindasilva <kss.kavinda@gmail.com>
2020-03-13 21:36:03 +0530

    WIP redux refactor

--- src/components/common/main.jsx
+++ src/components/common/main.jsx
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
-import { LoginPage } from '../LoginPage';
+import { HomePage } from '../common/home';
+import { LoginPage } from '../common/login';
 
 class App extends React.Component {
     constructor(props) {
@@ -49,4 +49,4 @@ function mapStateToProps(state) {
 }
 
 const connectedApp = connect(mapStateToProps)(App);
-export { connectedApp as App }; 
+export default connectedApp; 
